24613827cf60e7bc9e28f4d340d4c6b0651e7644
aomizu <marco.tylus24@gmail.com>
2026-05-22 23:45:18 +0900

    bump version to 2.5.3

--- Project.swift
+++ Project.swift
@@ -15,8 +15,8 @@ let project = Project(
     ),
     settings: .settings(
         base: [
-            "MARKETING_VERSION": "2.5.2",
-            "CURRENT_PROJECT_VERSION": "20502",
+            "MARKETING_VERSION": "2.5.3",
+            "CURRENT_PROJECT_VERSION": "20503",
             "SWIFT_VERSION": "6.0",
             "MACOSX_DEPLOYMENT_TARGET": "14.0",
             "DEVELOPMENT_TEAM": "",
