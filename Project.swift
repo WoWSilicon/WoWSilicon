5276d92627f26334f6580270eabadf22361d3717
aomizu <marco.tylus24@gmail.com>
2026-09-25 08:47:46 +0900

    prepare for 3.2.2

--- Project.swift
+++ Project.swift
@@ -15,8 +15,8 @@ let project = Project(
     ),
     settings: .settings(
         base: [
-            "MARKETING_VERSION": "3.2.1",
-            "CURRENT_PROJECT_VERSION": "30201",
+            "MARKETING_VERSION": "3.2.2",
+            "CURRENT_PROJECT_VERSION": "30202",
             "SWIFT_VERSION": "6.0",
             "MACOSX_DEPLOYMENT_TARGET": "14.0",
             "DEVELOPMENT_TEAM": "",
